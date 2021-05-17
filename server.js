6c3537d0da33c3cc2f35567c64a53ce021e9977b
Nihalroshan <nihalroshanpc@gmail.com>
2021-05-17 13:47:37 +0530

    committing starter files

---
+++ server.js
@@ -0,0 +1,28 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/index");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error(err));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
